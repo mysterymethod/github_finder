47d630a5adf479e52f9e54272b91880eacf69322
pranoy <chandra.pranay@yahoo.in>
2022-07-14 15:41:50 +0530

    complete about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,22 @@
 function About() {
   return (
-    <div>About</div>
+    <div className="about">
+      <h1 className='about__heading'>Github Finder</h1>
+      <p className='about__paragraph'>
+        A React app to search GitHub profiles and see profile details. This
+        project is part of the
+
+      </p>
+      <p className=''>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className=''>
+        Developed By : &nbsp;
+        <a className='text-white' href='https://www.linkedin.com/in/pranoy-chandra-28a6571b4/'>
+          Pranoy Chandra
+        </a>
+      </p>
+    </div>
   )
 }
 

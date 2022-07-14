e9d286295e5700aa77e8f4878ba0375bdc5ba95d
pranoy <chandra.pranay@yahoo.in>
2022-07-14 15:27:48 +0530

    footer complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Navbar from "./components/layouts/Navbar";
+import Footer from './components/layouts/Footer';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 
 function App() {
@@ -9,7 +13,34 @@ function App() {
       <Router>
         <div className='screen-container'>
           <Navbar />
-          <main>content</main>
+          <main>
+            <Routes>
+              <Route
+                exact
+                path='/'
+                element={<Home />}
+              />
+
+              <Route
+                exact
+                path='/about'
+                element={<About />}
+              />
+
+              <Route
+                exact
+                path='/notfound'
+                element={<NotFound />}
+              />
+
+              <Route
+                exact
+                path='/*'
+                element={<NotFound />}
+              />
+            </Routes>
+          </main>
+          <Footer />
         </div>
       </Router>
 

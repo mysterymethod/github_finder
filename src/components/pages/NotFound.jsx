fe932c2d1984415494f385eb24c6dc89c6950a90
pranoy <chandra.pranay@yahoo.in>
2022-07-14 15:52:21 +0530

    not found page complete

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+import {FaHome} from 'react-icons/fa';
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="notFound">
+      <h1 className='notFound__header'>Oops!</h1>
+      <h2 className='notFound__subHeader'>404 - Page not found!</h2>
+      <Link className='notFound__link' to='/'> <FaHome />&nbsp; back to home</Link>
+    </div>
   )
 }
 
